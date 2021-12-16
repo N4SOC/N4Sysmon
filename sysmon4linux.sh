2945c22f3f36df624f22c5bfe3d1c322ddb6e0a5
Martin Rothe <m.rothe@node4.co.uk>
2021-12-16 11:28:00 +0000

    Improved OS detection logic

--- sysmon4linux.sh
+++ sysmon4linux.sh
@@ -1,21 +1,32 @@
 if grep -i ubuntu /etc/os-release>/dev/null; then
+    echo "Running Ubuntu install commands"
     wget -q https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
     dpkg -i packages-microsoft-prod.deb
     apt update
     apt install sysinternalsebpf # Install EBPF prerequisitite
     apt install sysmonforlinux # Install sysmonforlinux
 elif grep -i debian /etc/os-release>/dev/null; then
+    echo "Running Debian install commands"
     wget https://packages.microsoft.com/config/debian/$(lsb_release -rs)/packages-microsoft-prod.deb # Download microsoft repository installer
     dpkg -i packages-microsoft-prod.deb
     apt update
     apt install sysinternalsebpf # Install EBPF prerequisitite
     apt install sysmonforlinux # Install sysmonforlinux
 elif grep -i CentOS /etc/os-release>/dev/null; then
-    rpm -Uvh https://packages.microsoft.com/config/centos/8/packages-microsoft-prod.rpm
+    echo "Running CentOS install commands"
+    rpm -Uvh https://packages.microsoft.com/config/centos/$(lsb_release -rs)/packages-microsoft-prod.rpm
     dnf install sysinternalsebpf
     dnf install sysmonforlinux
+elif grep -i "Red Hat" /etc/os-release>/dev/null; then
+    echo "Running Redhat install commands"
+    rpm -Uvh https://packages.microsoft.com/config/rhel/$(lsb_release -rs)/packages-microsoft-prod.rpm
+    dnf install sysinternalsebpf
+    dnf install sysmonforlinux
+elif grep -i Arch /etc/os-release>/dev/null; then
+    echo "Arch Linux Not supported"
+    exit 1
 else
-    echo "OS Not supported"
+    echo "Other OS Not supported"
     exit 1
 fi
 
